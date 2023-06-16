7dd790a7c7d3aaa627ed2ab895378a187a70748b
joetech12 <ifeanyi4umeh@gmail.com>
2023-06-16 17:43:45 +0100

    adjusted side drawer content layout.

--- src/components/DrawerContent/index.js
+++ src/components/DrawerContent/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import {DrawerContentScrollView} from '@react-navigation/drawer';
 import auth from '@react-native-firebase/auth';
-import {Linking, StyleSheet, Text} from 'react-native';
+import {
+  Linking,
+  StyleSheet,
+  Text,
+  Pressable,
+  View,
+  TouchableOpacity,
+} from 'react-native';
 import colors from '../../constants/colors';
 import {
   PRIVACY_POLICY_LINK,
@@ -18,25 +25,30 @@ function DrawerContent(props) {
 
   return (
     <DrawerContentScrollView {...props}>
-      <Text style={styles.link} onPress={() => navigation.navigate('Home')}>
-        Home
-      </Text>
-      <Text style={styles.link} onPress={() => navigation.navigate('Tasks')}>
-        Tasks
-      </Text>
-      <Text
-        style={styles.link}
-        onPress={() => Linking.openURL(PRIVACY_POLICY_LINK)}>
-        Privacy Policy
-      </Text>
-      <Text
-        style={styles.link}
-        onPress={() => Linking.openURL(TERMS_CONDITIONS_LINK)}>
-        Terms and Conditions
-      </Text>
-      <Text style={styles.link} onPress={logout}>
-        Log out
-      </Text>
+      <View style={styles.container}>
+        <View>
+          <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+            <Text style={styles.link}>Home</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('Tasks')}>
+            <Text style={styles.link}>Tasks</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => Linking.openURL(PRIVACY_POLICY_LINK)}>
+            <Text style={styles.link}>Privacy Policy</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => Linking.openURL(TERMS_CONDITIONS_LINK)}>
+            <Text style={styles.link}>Terms and Conditions</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={logout}>
+            <Text style={styles.link}>Log out</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.footerContainer}>
+          <Text style={styles.footerText}>Ifeanyi Umeh © 2023</Text>
+        </View>
+      </View>
     </DrawerContentScrollView>
   );
 }
@@ -46,8 +58,40 @@ const styles = StyleSheet.create({
     color: colors.black,
     fontWeight: '500',
     fontSize: 13,
-    margin: 8,
+    marginVertical: 12,
     marginHorizontal: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: colors.lightGrey,
+    borderRadius: 8,
+  },
+  container: {
+    paddingVertical: 20,
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    // alignItems: 'center',
+    // borderWidth: 1,
+    // borderColor: colors.lightGrey,
+    height: 800,
+  },
+  footerText: {
+    color: colors.Grey,
+    fontSize: 13,
+    opacity: 0.5,
+  },
+  footerContainer: {
+    // width: '100%',
+    // textAlign: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // borderWidth: 1,
+    // borderColor: colors.lightGrey,
+    color: colors.lightGrey,
+    fontSize: 12,
+    marginVertical: 12,
+    marginHorizontal: 16,
+    padding: 8,
   },
 });
 
